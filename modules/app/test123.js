0e95e0ba744ce3a6f9ffc9f52e2cca6ea1a3027c
syd <nmmilk@163.com>
2017-11-10 11:09:50 +0800

    12343

--- modules/app/test123.js
+++ modules/app/test123.js
@@ -1,5 +1,7 @@
 var $ = require('jquery');
-  $(".wrap").hover(function(){
-    $(this).animate({"width":"300px","height":"100px"},1000).find(".test").animate({"width":"100px","height":"100px","margin-left":"100px"},1000);
-    $(".wrap").not($(this)).animate({"width":"200px","height":"100px"},1000).find(".test").animate({"width":"80px","height":"80px","margin-left":"0px"},1000);
-    });
+$("#test").click(function () {
+  $(".testBox").append("<div class='hhh'>click</div>");
+});
+$(".testBox").on("click", ".hhh", function () {
+  alert(123);
+});
